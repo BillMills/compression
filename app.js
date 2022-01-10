546f62dc5310241b13ab1ccc3e72c1dda7fcb6eb
billmills <you@example.com>
2022-01-10 15:35:28 -0500

    validated bitmask packing

--- app.js
+++ app.js
@@ -56,92 +56,56 @@ let db = mongoose.connection;
 //////////////// end mongo config //////////////////
 
 const Profile = require('./models/profile');
-const Profilex = require('./models/profilex');
-
-//aggPipeline = [{$match: {date: {$gt: new Date('2020-01-01T00:00:00Z'), $lte: new Date('2020-04-01T00:00:00Z')}}}, {$match: {date_added: {$lt: new Date('2020-04-01T00:00:00Z')}}}]
-//aggPipeline = [{$match: {date: {$gt: new Date('2019-01-01T00:00:00Z'), $lte: new Date('2020-01-01T00:00:00Z')}}}, {$match: {date_added: {$lt: new Date('2020-04-01T00:00:00Z')}}}]
-// pipelines = [
-//   [{$match: {date: {$lte: new Date('2005-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2005-01-01T00:00:00Z'), $lte: new Date('2006-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2006-01-01T00:00:00Z'), $lte: new Date('2007-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2007-01-01T00:00:00Z'), $lte: new Date('2008-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2008-01-01T00:00:00Z'), $lte: new Date('2009-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2009-01-01T00:00:00Z'), $lte: new Date('2010-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2010-01-01T00:00:00Z'), $lte: new Date('2011-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2011-01-01T00:00:00Z'), $lte: new Date('2012-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2012-01-01T00:00:00Z'), $lte: new Date('2013-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2013-01-01T00:00:00Z'), $lte: new Date('2014-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2014-01-01T00:00:00Z'), $lte: new Date('2015-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2015-01-01T00:00:00Z'), $lte: new Date('2016-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2016-01-01T00:00:00Z'), $lte: new Date('2017-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2017-01-01T00:00:00Z'), $lte: new Date('2018-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2018-01-01T00:00:00Z'), $lte: new Date('2019-01-01T00:00:00Z')}}}],
-//   [{$match: {date: {$gt: new Date('2019-01-01T00:00:00Z'), $lte: new Date('2020-01-01T00:00:00Z')}}}]
-// ]
-
-//for(let p=0; p<pipelines.length; p++){
-// for (var d = new Date('2005-01-01T00:00:00Z'); d <= new Date('2006-01-01T00:00:00Z'); d.setDate(d.getDate() + 1)) {
-//   next = d.setDate(d.getDate() + 1)
-//   let query = Profile.aggregate([{$match: {date: {$gte: d, $lt: next}}}]);
-
-//   query.exec(function (err, profiles) {
-//     if (err){
-//       console.log({"code": 500, "message": "Server error"});
-//       return;
-//     }
-
-//     // // keep only profiles that have at least one of a requested core or bgc measurement
-//     // profiles = profiles.filter(item => (('measurements' in item && item.measurements !== null && (item.measurements.some(elt => Object.keys(elt).length!=0)))) || 
-//     //                                    (('bgcMeas' in item && item.bgcMeas !== null && (item.bgcMeas.some(elt => Object.keys(elt).length!=0)))))
-
-//     if(profiles.length == 0) {
-//       console.log({"code": 404, "message": "Not found: No matching results found in database."});
-//       return;
-//     }
-    
-//     console.log('n profiles: ', profiles.length)
-//     console.log('uncompressed: ', Buffer.byteLength(JSON.stringify(profiles)))
-//     for(i=0; i<profiles.length; i++){
-//       //profiles[i]._id = profiles[i]._id+'x'
-//       profiles[i].compressed = true
-//       //dx = new Date(profiles[i].date)
-//       //dx.setFullYear(9999)
-//       //profiles[i].date = dx
-//       if(profiles[i].measurements) profiles[i].measurements = profiles[i].measurements.map(x => measminify(profiles[i].station_parameters, x))
-//       if(profiles[i].bgcMeas) profiles[i].bgcMeas = profiles[i].bgcMeas.map(x => measminify(profiles[i].bgcMeasKeys.concat(profiles[i].bgcMeasKeys.map(k=>k+'_qc')), x))
-//       px = new Profilex(profiles[i])
-//       profiles[i] = px
-//     }
-//     Profilex.insertMany(profiles, function(err) {console.log(err)})
-//     console.log('compressed: ', Buffer.byteLength(JSON.stringify(profiles)))
-//   })
-// }
+const Profileb = require('./models/profileb');
 
 async function traverse(){
   //for await (const profile of Profile.find({date: {$lt: new Date('2005-01-01T00:00:00Z')}})) {
-  //for await (const profile of Profile.find({date: {$gte: new Date('2005-01-01T00:00:00Z'), $lt: new Date('2015-01-01T00:00:00Z')}})) {
-  for await (const profile of Profile.find({date: {$gte: new Date('2015-01-01T00:00:00Z')}})) {
+  for await (const profile of Profile.find({date: {$gte: new Date('2005-01-01T00:00:00Z'), $lt: new Date('2015-01-01T00:00:00Z')}})) {
+  //for await (const profile of Profile.find({date: {$gte: new Date('2015-01-01T00:00:00Z')}})) {
     p = profile.toObject()
-    p._id = p._id
-    dx = new Date(p.date)
-    dx.setFullYear(dx.getFullYear() + 1000)
-    p.date = dx
     if(p.measurements) {
-      p.measurements = p.measurements.map(x => measminify(p.station_parameters, x))
+      p.measurements = p.measurements.map(x => measminify(p.station_parameters.concat(p.station_parameters.map(k=>k+'_qc')), x))
     }
     if(p.bgcMeas){
       p.bgcMeas = p.bgcMeas.map(x => measminify(p.bgcMeasKeys.concat(p.bgcMeasKeys.map(k=>k+'_qc')), x))
     }
-    Profilex.insertMany([p], function(err) {console.log(err)})
+    Profileb.insertMany([p], function(err) {if(err) console.log(err)})
   }
 }
 traverse()
 
-measminify = function(varlist, measurement){
-  // turn a single measurement object into a values-only array, with values in the order specified by varlist
+bitmask = function(varlist, measurement){
+  // varlist: array of measurement keys
+  // measurement: a JSON object describing a single level measurement
+  // returns a bitmask represented as an array of 32-bit numbers, little-endian
+  // ie the least significant bit in bitmask[0] signifies the presence of varlist[0],
+  // the least significant bit in bitmask[1] signifies the presence of varlist[32], etc.
 
-  min = varlist.map(x => measurement[x]) 
-  //console.log(varlist, measurement, min)
+  mask = new Array(Math.ceil(varlist.length / 32)).fill(0);
+
+  for (const [key, value] of Object.entries(measurement)) {
+    if(value===null || isNaN(value)) continue
+    bit = varlist.findIndex((k) => k == key)
+    if(bit == -1) console.log(key, value, varlist)
+    index = (bit - bit%32)/32 // ie the array index in the bitmask
+    bit = bit%32
+    mask[index] = mask[index] | (1<<bit)
+  }
+
+  return mask
+}
+
+measminify = function(varlist, measurement){
+  // turn a single measurement object into a values-only array, with values in the order specified by varlist, and value presence indicated by the bitmask in the first array position
+
+  mask = bitmask(varlist, measurement)
+  min = [mask]
+  for(i=0; i<varlist.length; i++){
+    if(measurement.hasOwnProperty(varlist[i]) && !Number.isNaN(measurement[varlist[i]])){
+      min.push(measurement[varlist[i]])
+    }
+  }
+
   return min
 }
 
